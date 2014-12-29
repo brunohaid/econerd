0652b7c4324689fcc04e1949f3e90dc97b302987
Bruno <bruno.haid@gmail.com>
2014-12-29 11:00:37 +0800

    Remove redundant request params

--- crawler/process.go
+++ crawler/process.go
@@ -1,21 +1,51 @@
 package crawler
 
-import "log"
+import (
+	"log"
+	"strings"
+)
 
 var (
 	// Cache processed IDs
 	knownids map[string]bool
+
+	// URL endings we remove
+	trimmings = [...]string{ 
+		// Analytics
+		"utm_",
+		// Reuters source
+		"feedType=RSS",
+		// NYtimes sharing
+		"hp&amp;action" }	
 )
 
 // If a new item was found
 func AddItem(item Item) {
 	// Log it
-	log.Printf("ITEM on %s: %s",item.kind,item.url)	
+	log.Printf("ITEM on %s: %s",item.kind,trimurl(item.url))
 }
 
 // If a new mention was found
 func AddMention(mention Mention) {
 	// Log it
-	log.Printf("MENTION on %s: %s",mention.kind,mention.target)	
+	log.Printf("MENTION on %s: %s",mention.kind,trimurl(mention.target))
+}
+
+// Remove redundant parts from url string
+func trimurl(url string) (trimmed string) {
+	// Iterate through our trim vars
+	for _, pattern := range trimmings {
+		// See if we find a a pattern
+		if i := strings.Index(url,"?" + pattern); i > -1 {
+			url = url[:i]
+		}
+	}
+	return url
+}
+
+// Takes URL and tries to reduce it to a canonical core
+// This does not have to be a valid URL anymore, just a unique identifier
+func GetCanoncical(s string) string {
+	return s
 }
 
--- crawler/util.go
+++ crawler/util.go
@@ -3,11 +3,6 @@ package crawler
 import (
 	// Core
 	"time"
-	"log"
-
-	// Stream processing
-	"io/ioutil"
-	"encoding/json"
 
 	// Interwebs!
 	"net/http"
@@ -54,48 +49,4 @@ func fetchurl(url string) ( *http.Response, error) {
 	client := gethttpclient()
 
 	return client.Get(url)
-}
-
-// Get the contents of a specific page
-func gethtml( url string ) string {
-	if response, err := fetchurl(url); err != nil {
-		// If fetching failed
-		log.Printf("Error fetching url %s: %+v", url, err)
-
-		// Return empty if something failed
-    	return ""
-	} else {
-		// Always close the body after we read it
-        defer response.Body.Close()
-
-        // Read the actual contents, ignore errors for now
-        body, _ := ioutil.ReadAll(response.Body)
-        
-        // Return the contents, converting byte array to string
-        return string(body)
-	}	
-}
-
-// Fetch a JSON url and return it as generic interface
-func getjson(url string) ( i interface{}, err error ) {
-	// Fetch the URL
-	response, err := fetchurl(url); 
-
-	// If we didn't get any data
-	if err != nil {
-		// Log an error
-		log.Printf("Error fetching JSON %s: %+v", url, err)
-	} else {
-		// Decode bytes b into interface i, override nil error if something goes wrong
-		err = json.NewDecoder(response.Body).Decode(&i)
-	}
-
-	// Return the interface
-	return i, err
-}
-
-// Takes URL and tries to reduce it to a canonical core
-// This does not have to be a valid URL anymore, just a unique identifier
-func GetCanoncical(s string) string {
-	return s
 }
